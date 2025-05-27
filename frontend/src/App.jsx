8a5764536dbabaff15a0f948e38a0e6df3d02e59
vishal pathak <vishalpathak842@gmail.com>
2025-05-27 14:14:45 +0530

    Implement routing in App component with HomePage, LoginPage, and ProfilePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import ProfilePage from './pages/ProfilePage'
+import LoginPage from './pages/LoginPage'
 
 const App = () => {
   return (
-    <div className='bg-gray-500'>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/profile' element={<ProfilePage/>}/>
+      </Routes>
+    </div>
   )
 }
 
